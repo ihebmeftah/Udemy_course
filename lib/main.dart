5602003fc6e179e1182fc879c3f8e73c818ea707
iheb meftah <salimbb041@gmail.com>
2021-12-20 15:28:53 +0100

    clean

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hexcolor/hexcolor.dart';
 import 'package:newsapp/layout/newslayout/news_app/cubit/cubit.dart';
 import 'package:newsapp/shared/Appcubit/cubit.dart';
 import 'package:newsapp/shared/Appcubit/states.dart';
 import 'package:newsapp/shared/blocObserver.dart';
 import 'package:newsapp/shared/network/local/cache_helper.dart';
 import 'package:newsapp/shared/network/remote/diohelper.dart';
+import 'package:newsapp/shared/styles/theme.dart';
 import 'layout/newslayout/news_app/news_layout.dart';
 
 void main() async {
@@ -41,53 +40,8 @@ class MyApp extends StatelessWidget {
         builder: (context, state) {
           return MaterialApp(
             title: 'Flutter Demo',
-            theme: ThemeData(
-                textTheme: TextTheme(
-                    bodyText1: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600)),
-                primarySwatch: Colors.orange,
-                scaffoldBackgroundColor: Colors.white,
-                appBarTheme: const AppBarTheme(
-                  titleSpacing: 20.0,
-                  titleTextStyle: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                  backgroundColor: Colors.white,
-                  elevation: 0.0,
-                  systemOverlayStyle: SystemUiOverlayStyle(
-                      statusBarColor: Colors.white,
-                      statusBarIconBrightness: Brightness.dark),
-                ),
-                bottomNavigationBarTheme: BottomNavigationBarThemeData(
-                    type: BottomNavigationBarType.fixed)),
-            darkTheme: ThemeData(
-                textTheme: TextTheme(
-                    bodyText1: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600)),
-                scaffoldBackgroundColor: HexColor('333739'),
-                primarySwatch: Colors.orange,
-                appBarTheme: AppBarTheme(
-                  titleSpacing: 20.0,
-                  iconTheme: IconThemeData(color: Colors.white),
-                  titleTextStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                  backgroundColor: HexColor('333739'),
-                  elevation: 0.0,
-                  systemOverlayStyle: SystemUiOverlayStyle(
-                      statusBarColor: HexColor('333739'),
-                      statusBarIconBrightness: Brightness.light),
-                ),
-                bottomNavigationBarTheme: BottomNavigationBarThemeData(
-                    backgroundColor: HexColor('333739'),
-                    unselectedItemColor: Colors.grey,
-                    type: BottomNavigationBarType.fixed)),
+            theme: lightTheme,
+            darkTheme: darkTheme,
             themeMode:
                 AppCubit.get(context).isDark ? ThemeMode.dark : ThemeMode.light,
             home: const NewsLayout(),
